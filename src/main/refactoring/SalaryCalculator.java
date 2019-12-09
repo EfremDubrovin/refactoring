0b187f3cf1714b1fb6e7f7306250b93bec3f2b3f
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2019-12-10 00:12:03 +0200

    added duplication

--- src/main/refactoring/SalaryCalculator.java
+++ src/main/refactoring/SalaryCalculator.java
@@ -19,28 +19,34 @@ public class SalaryCalculator {
             throw new IllegalArgumentException("Unsupported number of arguments, expecting single file path");
         }
         String row;
-        // Calculate each person's salary
-        double x = 0.0;
         Map<Integer, Worker> dataMap = new HashMap<>();
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
             System.out.print(String.format("%s %s earned: %s \n", data[1], data[2], data[3]));
-            x += Double.parseDouble(data[3]);
             Worker tempWorker = dataMap.get(Integer.parseInt(data[0]));
             if (tempWorker == null) {
                 tempWorker = new Worker(Integer.parseInt(data[0]), data[1], data[2]);
-                tempWorker.addEarning(Double.parseDouble(data[3]));
+                tempWorker.totalEarnings.add(Double.parseDouble(data[3]));
                 dataMap.put(Integer.parseInt(data[0]), tempWorker);
             } else {
-                tempWorker.addEarning(Double.parseDouble(data[3]));
+                tempWorker.totalEarnings.add(Double.parseDouble(data[3]));
             }
         }
         csvReader.close();
-        dataMap.forEach((integer, worker) -> {
-            Double sumForWorker = worker.totalEarnings.stream()
-                    .reduce(0.0, (aDouble, aDouble2) -> aDouble + aDouble2);
+        for (Worker worker : dataMap.values()) {
+            Double sumForWorker = 0.0;
+            for (Double money : worker.totalEarnings) {
+                sumForWorker += money;
+            }
             System.out.println(String.format("Total earnings for worker %s %.2f", worker.firstName, sumForWorker));
-        });
+        }
+
+        double x = 0.0;
+        for (Worker worker : dataMap.values()) {
+            for (Double money : worker.totalEarnings) {
+                x += money;
+            }
+        }
         System.out.println(String.format("Total earnings for all workers %.2f", x));
     }
 
@@ -50,14 +56,10 @@ public class SalaryCalculator {
         private String lastName;
         private List<Double> totalEarnings = new ArrayList<>();
 
-        public Worker(Integer id, String firstName, String lastName) {
+        Worker(Integer id, String firstName, String lastName) {
             this.id = id;
             this.firstName = firstName;
             this.lastName = lastName;
-        }
-
-        public void addEarning(Double earning) {
-            totalEarnings.add(earning);
         }
     }
 }
