70c45a99e0b0e9b4de859a1fc7f8cf4001cd281e
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2019-12-11 12:21:10 +0200

    first session

--- src/main/refactoring/SalaryCalculator.java
+++ src/main/refactoring/SalaryCalculator.java
@@ -8,32 +8,27 @@ import java.util.Map;
 
 public class SalaryCalculator {
 
+    public static final int FIRST_NAME_INDEX = 1;
+    public static final int LAST_NAME_INDEX = 2;
+    public static final int WORKER_EARNING_INDEX = 3;
+    public static final int ID_INDEX = 0;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader csvReader;
-        if (args.length < 1) {
-            InputStream resourceAsStream = SalaryCalculator.class.getResourceAsStream("/resources/account_data.csv");
-            csvReader = new BufferedReader(new InputStreamReader(resourceAsStream));
-        } else if (args.length == 1) {
-            csvReader = new BufferedReader(new FileReader(args[0]));
-        } else {
-            throw new IllegalArgumentException("Unsupported number of arguments, expecting single file path");
-        }
+        BufferedReader csvReader = readCsv(args);
         String row;
-        Map<Integer, Worker> dataMap = new HashMap<>();
+        Map<Integer, Worker> workers = new HashMap<>();
         while ((row = csvReader.readLine()) != null) {
-            String[] data = row.split(",");
-            System.out.print(String.format("%s %s earned: %s \n", data[1], data[2], data[3]));
-            Worker tempWorker = dataMap.get(Integer.parseInt(data[0]));
+            String[] readRow = row.split(",");
+            System.out.print(String.format("%s %s earned: %s \n", readRow[FIRST_NAME_INDEX], readRow[LAST_NAME_INDEX], readRow[WORKER_EARNING_INDEX]));
+            Worker tempWorker = workers.get(Integer.parseInt(readRow[ID_INDEX]));
             if (tempWorker == null) {
-                tempWorker = new Worker(Integer.parseInt(data[0]), data[1], data[2]);
-                tempWorker.totalEarnings.add(Double.parseDouble(data[3]));
-                dataMap.put(Integer.parseInt(data[0]), tempWorker);
-            } else {
-                tempWorker.totalEarnings.add(Double.parseDouble(data[3]));
+                tempWorker = new Worker(Integer.parseInt(readRow[ID_INDEX]), readRow[FIRST_NAME_INDEX], readRow[LAST_NAME_INDEX]);
             }
+            tempWorker.addEarning(parseSalary(readRow));
+            workers.put(Integer.parseInt(readRow[ID_INDEX]), tempWorker);
         }
         csvReader.close();
-        for (Worker worker : dataMap.values()) {
+        for (Worker worker : workers.values()) {
             Double sumForWorker = 0.0;
             for (Double money : worker.totalEarnings) {
                 sumForWorker += money;
@@ -42,12 +37,27 @@ public class SalaryCalculator {
         }
 
         double x = 0.0;
-        for (Worker worker : dataMap.values()) {
+        for (Worker worker : workers.values()) {
             for (Double money : worker.totalEarnings) {
                 x += money;
             }
         }
         System.out.println(String.format("Total earnings for all workers %.2f", x));
+    }
+
+    private static Double parseSalary(String[] row) {
+        return Double.parseDouble(row[WORKER_EARNING_INDEX]);
+    }
+
+    public static BufferedReader readCsv(String[] args) throws FileNotFoundException {
+        if (args.length < 1) {
+            InputStream resourceAsStream = SalaryCalculator.class.getResourceAsStream("/resources/account_data.csv");
+            return new BufferedReader(new InputStreamReader(resourceAsStream));
+        } else if (args.length == 1) {
+            return new BufferedReader(new FileReader(args[0]));
+        } else {
+            throw new IllegalArgumentException("Unsupported number of arguments, expecting single file path");
+        }
     }
 
     private static class Worker {
@@ -60,6 +70,10 @@ public class SalaryCalculator {
             this.id = id;
             this.firstName = firstName;
             this.lastName = lastName;
+        }
+
+        public void addEarning(Double earning){
+            this.totalEarnings.add(earning);
         }
     }
 }
