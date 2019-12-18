fc41b1b60234081cf7584475677639a05e256728
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2019-12-18 09:05:02 +0200

    session #2

--- src/main/refactoring/SalaryCalculator.java
+++ src/main/refactoring/SalaryCalculator.java
@@ -15,38 +15,50 @@ public class SalaryCalculator {
 
     public static void main(String[] args) throws IOException {
         BufferedReader csvReader = readCsv(args);
+        List<Worker> workers = getWorkersFromCsv(csvReader);
+        csvReader.close();
+        printAccumulatedResults(getAccumulatedWorkersPoints(workers));
+    }
+
+    private static void printAccumulatedResults(Map<Integer, Worker> distinctWorkers) {
+        double totalPoints = 0.0;
+        for (Worker worker : distinctWorkers.values()) {
+            totalPoints += worker.getPoints();
+            System.out.println(String.format("Total earnings for worker %s %.2f", worker.getFirstName(), worker.getPoints()));
+        }
+        System.out.println(String.format("Total earnings for all workers %.2f", totalPoints));
+    }
+
+    private static Map<Integer, Worker> getAccumulatedWorkersPoints(List<Worker> workers) {
+        Map<Integer, Worker> distinctWorkers = new HashMap<>();
+        for (Worker worker : workers) {
+            Worker distinctWorker = distinctWorkers.get(worker.getId());
+            if (distinctWorker != null) {
+                distinctWorker.addPoints(worker.getPoints());
+            } else {
+                distinctWorkers.put(worker.getId(), worker);
+            }
+        }
+        return distinctWorkers;
+    }
+
+    private static List<Worker> getWorkersFromCsv(BufferedReader csvReader) throws IOException {
         String row;
-        Map<Integer, Worker> workers = new HashMap<>();
+        List<Worker> workers = new ArrayList<>();
         while ((row = csvReader.readLine()) != null) {
             String[] readRow = row.split(",");
             System.out.print(String.format("%s %s earned: %s \n", readRow[FIRST_NAME_INDEX], readRow[LAST_NAME_INDEX], readRow[WORKER_EARNING_INDEX]));
-            Worker tempWorker = workers.get(Integer.parseInt(readRow[ID_INDEX]));
-            if (tempWorker == null) {
-                tempWorker = new Worker(Integer.parseInt(readRow[ID_INDEX]), readRow[FIRST_NAME_INDEX], readRow[LAST_NAME_INDEX]);
-            }
-            tempWorker.addEarning(parseSalary(readRow));
-            workers.put(Integer.parseInt(readRow[ID_INDEX]), tempWorker);
+            workers.add(new Worker(parseId(readRow), readRow[FIRST_NAME_INDEX], readRow[LAST_NAME_INDEX], parseSalary(readRow)));
         }
-        csvReader.close();
-        for (Worker worker : workers.values()) {
-            Double sumForWorker = 0.0;
-            for (Double money : worker.totalEarnings) {
-                sumForWorker += money;
-            }
-            System.out.println(String.format("Total earnings for worker %s %.2f", worker.firstName, sumForWorker));
-        }
-
-        double x = 0.0;
-        for (Worker worker : workers.values()) {
-            for (Double money : worker.totalEarnings) {
-                x += money;
-            }
-        }
-        System.out.println(String.format("Total earnings for all workers %.2f", x));
+        return workers;
     }
 
     private static Double parseSalary(String[] row) {
         return Double.parseDouble(row[WORKER_EARNING_INDEX]);
+    }
+
+    private static Integer parseId(String[] row) {
+        return Integer.parseInt(row[ID_INDEX]);
     }
 
     public static BufferedReader readCsv(String[] args) throws FileNotFoundException {
@@ -64,16 +76,33 @@ public class SalaryCalculator {
         private Integer id;
         private String firstName;
         private String lastName;
-        private List<Double> totalEarnings = new ArrayList<>();
+        private Double points;
 
-        Worker(Integer id, String firstName, String lastName) {
+        Worker(Integer id, String firstName, String lastName, Double points) {
             this.id = id;
             this.firstName = firstName;
             this.lastName = lastName;
+            this.points = points;
         }
 
-        public void addEarning(Double earning){
-            this.totalEarnings.add(earning);
+        public void addPoints(Double points) {
+            this.points += points;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public Double getPoints() {
+            return points;
         }
     }
 }
